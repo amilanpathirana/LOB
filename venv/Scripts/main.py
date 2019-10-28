c5abedab10c68208609ac417000d87072524284b
Amila PAthirana <emailpathirana@gmail.com>
2019-10-27 20:49:39 -0500

    'One box moving around. stop when reward '

--- venv/Scripts/main.py
+++ venv/Scripts/main.py
@@ -7,8 +7,6 @@ bluebox_img = pg.image.load("C:/Users/Nilushie/Desktop/BOX/img/blue_box.png")
 greenbox_img = pg.image.load("C:/Users/Nilushie/Desktop/BOX/img/green_box.png")
 ashbox_img = pg.image.load("C:/Users/Nilushie/Desktop/BOX/img/ash_box.png")
 
-
-
 def drawbluebox(x, y):
     display.blit(bluebox_img, (x * W, y * W))
 
@@ -28,7 +26,6 @@ def moveR(Tile):
         grid[x1][y1].item = True
         grid[x1-1][y1-1].item=False
         display.blit(ashbox_img, (x1 * W, y1 * W))
-
 
 
 W=40
@@ -51,57 +48,85 @@ pg.init()
 display=pg.display.set_mode((display_width,display_height))
 pg.display.set_caption("Box")
 clock = pg.time.Clock()
+grid=[[0 for n in range(9)] for n in range(9)]
 
 def gameloop():
     crashed=False
     display.fill(white)
-    grid=[[Tile() for n in range(9)] for n in range(9)]
+
+
+
+    x1=8
+    y1=8
+
     random.seed(20)
-    x = randint(0, 8)
-    y = randint(0, 8)
+    '''x = randint(0, 8)
+    y = randint(0, 8)'''
+    x=4
+    y=4
     dx=0
     dy=0
-
+    reward = 0
     while not crashed:
+
         for event in pg.event.get():
             if event.type==pg.QUIT:
                 crashed=True
             if event.type==pg.KEYDOWN:
                 if event.key==pg.K_LEFT:
                     dx=-1
+                    action=0
                 elif event.key==pg.K_RIGHT:
                     dx=1
+                    action = 1
                 elif event.key==pg.K_UP:
                     dy=-1
+                    action = 2
                 elif event.key==pg.K_DOWN:
                     dy=1
+                    action = 3
 
-        x = x + dx
-        y = y + dy
+                grid[x][y]=0
+                x = x + dx
+                y = y + dy
 
-        if x==9 or y==9 or x==-1 or y==-1 :
-            print("limit reached")
-            x = x - dx
-            y = y - dy
-            dx = 0
-            dy = 0
-        else:
-            dx = 0
-            dy = 0
+                if x==9:
+                    print("limit reached")
+                    x = x - dx
+                elif y==9:
+                    print("limit reached")
+                    y=y-dy
+                elif y==-1:
+                    print("limit reached")
+                    y=y-dy
+                elif x==-1:
+                    print("limit reached")
+                    x=x-dx
+                dx=0
+                dy=0
+                if grid[x][y]==2:
+                    grid[x][y] = 1
+                    reward=1
+                    crashed=True
+                else:
+                    reward=0
+
+
+                print(x,y)
 
         display.fill(white)
+        drawbluebox(x1, y1)
+        grid[x1][y1]=2
         drawgreenbox(x, y)
-        print(event)
+        '''print(event)'''
+
         pg.display.update()
         clock.tick(10)
 
+    return
 
-
-gameloop()
+reward=gameloop()
+print(reward)
 pg.quit()
 quit()
 
-'''    drawbluebox(x,y)
-    drawgreenbox(x+320, y)
-    pg.display.update()
-    clock.tick(60)'''
